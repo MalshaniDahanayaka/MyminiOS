f6d0a31b5654a3d084da96a4ddf8d96c6db41622
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2021-08-13 12:58:57 +0530

    Updated frame_buffer.c file

--- drivers/frame_buffer/frame_buffer.h
+++ drivers/frame_buffer/frame_buffer.h
@@ -1,9 +1,6 @@
 #ifndef INCLUDE_FRAMEBUFFER_H
 #define INCLUDE_FRAMEBUFFER_H
 
-#pragma once
-#include "io.h"
-
 /* The I/O ports */
 #define FB_COMMAND_PORT 0x3D4
 #define FB_DATA_PORT 0x3D5
@@ -19,32 +16,6 @@
 /* Frame buffer base address */
 #define FB_BASE_ADDRESS 0xB8000
 
-/** outb:
- *  Sends the given data to the given I/O port. Defined in io.s
- *
- *  @param port The I/O port to send the data to
- *  @param data The data to send to the I/O port
- */
-void outb(unsigned short port, unsigned char data);
-
-/** fb_move_cursor:
- *  Moves the cursor of the framebuffer to the given position
- *
- *  @param pos The new position of the cursor
- */
-void fb_move_cursor(unsigned short pos);
-
-/** fb_write_cell:
- *  Writes a character with the given foreground and background to position i
- *  in the framebuffer.
- *
- *  @param i  The location in the framebuffer
- *  @param c  The character
- *  @param fg The foreground color
- *  @param bg The background color
- */
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
-
 /** write:
  *  writes the contents of the buffer buf of length len to the screen
  *
@@ -53,4 +24,4 @@ void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
  */
 int fb_write(char *buf, unsigned int len);
 
-#endif /* INCLUDE_IO_H */
+#endif /* INCLUDE_FRAMEBUFFER_H */
