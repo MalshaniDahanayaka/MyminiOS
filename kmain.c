82d593d28316eebef832957ad95707a4682c4c54
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2021-08-20 15:08:38 +0530

    add kernel files

--- kmain.c
+++ kmain.c
@@ -1,21 +1,32 @@
-#include "segmentation/gdt.h"
-#include "drivers/serial_port/serial_port.h"
-#include "drivers/frame_buffer/frame_buffer.h"
+#include "gdt.h"
+#include "idt.h"
+#include "logger.h"
+#include "serial_port.h"
+#include "timer.h"
+#include "types.h"
 
 /* Function to initialize */
 void init() {
   /* Initialize segment descriptor tables */
   init_gdt();
 
+  /* Initialize segment descriptor tables */
+  init_idt();
+
+  /* Initialize timer interrupt */
+  init_timer(TIMER_FREQUENCY);
+
   /* Initialize serial port */
   serial_configure(SERIAL_COM1_BASE, Baud_115200);
 }
 
 /* Kernel Main */
-int kmain() {
+s32int kmain() {
   init();
-  char buffer[50] = "Welcome to MyminiOS...This works!!!!!!!\n";
-  fb_write(buffer, 50);
-  serial_write(SERIAL_COM1_BASE, buffer, 50);
+  s8int buffer[20] = "This works!!!!!!!\n";
+  print_screen(buffer, 20);
+  print_serial(buffer, 20);
+  asm volatile("int $0x3");
+  asm volatile("int $0x22");
   return 0;
 }
